5866b8aa9886f32dedd8fca8a16d2e2d2d3d565f
Ari Iwunze <62076939+ariavathlete@users.noreply.github.com>
2020-11-01 10:36:57 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler,OneHotEncoder, LabelEncoder
 
 
-lb = MultiLabelBinarizer()
-scaler = StandardScaler(with_mean=False)
+lb = OneHotEncoder()
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+numerical = ['SPEED LIMIT', 'AGE']
+categorical = ['DRIVER VISION', 'DRIVER ACTION','PHYSICAL CONDITION',
+                                'ROADWAY CONDITION','DEVICE CONDITION','FIRST CRASH TYPE']
 
 CORS(app)
 
@@ -77,22 +79,29 @@ def model_predict(x, model):
                                  'ROADWAY CONDITION','DEVICE CONDITION','FIRST CRASH TYPE']
 
     # input = pd.get_dummies(x, columns = CT, drop_first = True)
+    preds = pd.get_dummies(x, columns = CT, drop_first = True)
+
+
+    fname = 'scaler.sav'
+    scaler = pickle.load(open(fname, 'rb'))
     
-    preds = lb.fit_transform(x[CT])
+    scaler.fit(preds)
+    preds = scaler.transform(preds)
 
+    filename = 'xgb.sav'
+    model = pickle.load(open(filename, 'rb'))
 
-    preds = scaler.fit_transform(preds)
-
-
+    preds = np.array(preds)
     
-    preds = np.array(preds).reshape(-1,1)
+
+    # preds = np.array(preds).reshape(1,-1)
 
 
-
-    preds = model.predict_proba(preds)
+    # preds = np.ravel(preds)
+    preds = model.predict(preds)[0]
     
-    preds = np.ravel(preds)
-    preds = np.argpartition(preds, -1)[-1:]
+    
+    # preds = np.argpartition(preds, -1)[-1:]
 
     #preds = np.argsort(preds, axis=1)[:,-3:]
     def largest_indices(ary, n):
@@ -113,6 +122,124 @@ def model_predict(x, model):
 def upload_file2():
     return render_template('index.html', title='Home')
 
+def analysis(speed, age, vision, action, physical, roadway, device, fcrash):
+    predictions = []
+    recommend_index = []
+    recommend = []
+    column = ['SPEED LIMIT', 'AGE','DRIVER VISION', 'DRIVER ACTION','PHYSICAL CONDITION',
+                                'ROADWAY CONDITION','DEVICE CONDITION','FIRST CRASH TYPE']
+    
+    serInput = [speed,age,vision,action,physical,roadway,device,fcrash]
+    
+    data = pd.DataFrame([serInput], columns=column)
+    
+    df_num = data[numerical]
+    df_cat = data[categorical]
+    #df_cat.columns = [''] * len(df_cat.columns)
+    df_cat = pd.get_dummies(df_cat)
+    #df_cat.columns = df_cat.columns.str[1:]
+    df_missing = pd.concat([df_num, df_cat], axis=1)
+    
+
+
+    filename = 'xgb.sav'
+    model = pickle.load(open(filename, 'rb'))
+
+    cols_when_model_builds = ['SPEED LIMIT', 'AGE',
+       'DRIVER VISION_BLINDED - SUNLIGHT', 'DRIVER VISION_BLOWING MATERIALS',
+       'DRIVER VISION_BUILDINGS', 'DRIVER VISION_EMBANKMENT',
+       'DRIVER VISION_HILLCREST', 'DRIVER VISION_MOVING VEHICLES',
+       'DRIVER VISION_NOT OBSCURED', 'DRIVER VISION_OTHER',
+       'DRIVER VISION_PARKED VEHICLES', 'DRIVER VISION_SIGNBOARD',
+       'DRIVER VISION_TREES, PLANTS', 'DRIVER VISION_UNKNOWN',
+       'DRIVER VISION_WINDSHIELD (WATER/ICE)',
+       'DRIVER ACTION_DISREGARDED CONTROL DEVICES',
+       'DRIVER ACTION_EMERGENCY VEHICLE ON CALL',
+       'DRIVER ACTION_EVADING POLICE VEHICLE', 'DRIVER ACTION_FAILED TO YIELD',
+       'DRIVER ACTION_FOLLOWED TOO CLOSELY', 'DRIVER ACTION_IMPROPER BACKING',
+       'DRIVER ACTION_IMPROPER LANE CHANGE', 'DRIVER ACTION_IMPROPER PARKING',
+       'DRIVER ACTION_IMPROPER PASSING', 'DRIVER ACTION_IMPROPER TURN',
+       'DRIVER ACTION_LICENSE RESTRICTIONS', 'DRIVER ACTION_NONE',
+       'DRIVER ACTION_OTHER', 'DRIVER ACTION_OVERCORRECTED',
+       'DRIVER ACTION_STOPPED SCHOOL BUS', 'DRIVER ACTION_TEXTING',
+       'DRIVER ACTION_TOO FAST FOR CONDITIONS', 'DRIVER ACTION_UNKNOWN',
+       'DRIVER ACTION_WRONG WAY/SIDE', 'PHYSICAL CONDITION_FATIGUED/ASLEEP',
+       'PHYSICAL CONDITION_HAD BEEN DRINKING',
+       'PHYSICAL CONDITION_ILLNESS/FAINTED',
+       'PHYSICAL CONDITION_IMPAIRED - ALCOHOL',
+       'PHYSICAL CONDITION_IMPAIRED - ALCOHOL AND DRUGS',
+       'PHYSICAL CONDITION_IMPAIRED - DRUGS', 'PHYSICAL CONDITION_MEDICATED',
+       'PHYSICAL CONDITION_NORMAL', 'PHYSICAL CONDITION_OTHER',
+       'PHYSICAL CONDITION_REMOVED BY EMS', 'PHYSICAL CONDITION_UNKNOWN',
+       'ROADWAY CONDITION_ICE', 'ROADWAY CONDITION_OTHER',
+       'ROADWAY CONDITION_SAND, MUD, DIRT', 'ROADWAY CONDITION_SNOW OR SLUSH',
+       'ROADWAY CONDITION_UNKNOWN', 'ROADWAY CONDITION_WET',
+       'DEVICE CONDITION_FUNCTIONING PROPERLY', 'DEVICE CONDITION_MISSING',
+       'DEVICE CONDITION_NO CONTROLS', 'DEVICE CONDITION_NOT FUNCTIONING',
+       'DEVICE CONDITION_OTHER', 'DEVICE CONDITION_UNKNOWN',
+       'DEVICE CONDITION_WORN REFLECTIVE MATERIAL', 'FIRST CRASH TYPE_ANIMAL',
+       'FIRST CRASH TYPE_FIXED OBJECT', 'FIRST CRASH TYPE_HEAD ON',
+       'FIRST CRASH TYPE_OTHER NONCOLLISION', 'FIRST CRASH TYPE_OTHER OBJECT',
+       'FIRST CRASH TYPE_OVERTURNED', 'FIRST CRASH TYPE_PARKED MOTOR VEHICLE',
+       'FIRST CRASH TYPE_PEDALCYCLIST', 'FIRST CRASH TYPE_PEDESTRIAN',
+       'FIRST CRASH TYPE_REAR END', 'FIRST CRASH TYPE_REAR TO FRONT',
+       'FIRST CRASH TYPE_REAR TO REAR', 'FIRST CRASH TYPE_REAR TO SIDE',
+       'FIRST CRASH TYPE_SIDESWIPE OPPOSITE DIRECTION',
+       'FIRST CRASH TYPE_SIDESWIPE SAME DIRECTION', 'FIRST CRASH TYPE_TRAIN',
+       'FIRST CRASH TYPE_TURNING']
+    
+    # deal with missing columns
+    missing_cols = set(cols_when_model_builds) - set(df_missing.columns)
+    for col in missing_cols:
+         df_missing[col] = 0
+    df = df_missing[cols_when_model_builds]
+
+
+    from joblib import dump, load
+    scaler = load('std_scaler.bin')
+
+    # scaler.fit(preds)
+    df = scaler.transform(df)
+
+    # df = pd.DataFrame(df, columns=cols_when_model_builds)
+    
+    # predict
+    result =  model.predict(df)
+    fixed_name =  {'FAILING TO YIELD RIGHTOFWAY': 'FAILING TO YIELD RIGHT-OF-WAY',
+     'FOLLOWING TOO CLOSELY': 'FOLLOWING TOO CLOSELY',
+     'SPEED RELATED': 'SPEED RELATED',
+      'IMPROPER BACKING': 'IMPROPER BACKING',
+      'IMPROPER OVERTAKINGPASSING': 'IMPROPER OVERTAKING OR PASSING',
+
+      'IMPROPER LANE USAGE': 'IMPROPER LANE USAGE',
+      'IMPROPER TURNINGNO SIGNAL': 'IMPROPER TURNINGNO SIGNAL',
+
+      'DISREGARDING TRAFFIC SIGNALS': 'DISREGARDING TRAFFIC SIGNALS',
+      'DRIVING SKILLSKNOWLEDGEEXPERIENCE': 'DRIVING SKILLS, KNOWLEDGE OR EXPERIENCE',
+      'INTOXICATED/PHYSICAL CONDITION': 'INTOXICATED OR CURRENT PHYSICAL CONDITION RELATED',
+      'WEATHER': 'WEATHER',
+        'DISTRACTION  FROM OUTSIDE VEHICLE': 'DISTRACTION FROM OUTSIDE THE VEHICLE',
+      'DISTRACTION  FROM INSIDE VEHICLE': 'DISTRACTION FROM INSIDE THE VEHICLE',
+    'ROAD CONDITIONS': 'ROAD CONDITIONS',
+      'EQUIPMENT  VEHICLE CONDITION': 'EQUIPMENT VEHICLE CONDITION'}
+
+    for name, corr_name in fixed_name.items():
+        if name in result:
+            result = corr_name
+
+    
+    # for x in result:
+    #     max = sorted(x)
+    #      # x.argsort()[0]
+    #     if x[max]>0.6:
+    #         predictions.append(max)
+    #     elif max == 0:
+    #         predictions.append(max)
+    #     else:
+    #         predictions.append(max-1)
+
+    return result
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
@@ -128,21 +255,36 @@ def upload():
         device = request.args['DEVICE CONDITION']
         fcrash = request.args['FIRST CRASH TYPE']
         filename = 'xgb.sav'
-        #serInput = [speed,age,vision,action,physical,roadway,device,fcrash]
+
+        serInput = [speed,age,vision,action,physical,roadway,device,fcrash]
+
         model = pickle.load(open(filename, 'rb'))
-        data = {'SPEED LIMIT':[speed],'AGE':[age],'DRIVER VISION':[vision],'DRIVER ACTION':[action],
-         'PHYSICAL CONDITION':[physical],'ROADWAY CONDITION':[roadway],'DEVICE CONDITION':[device],'FIRST CRASH TYPE':[fcrash]}
 
-        input = pd.DataFrame(data, columns = ['SPEED LIMIT','AGE','DRIVER VISION','DRIVER ACTION','PHYSICAL CONDITION',
-                                 'ROADWAY CONDITION','DEVICE CONDITION','FIRST CRASH TYPE'])
-         
-        preds = model_predict(input, model)
+        result =  analysis(speed, age, vision, action, physical, roadway, device, fcrash)
+           
+        # data=pd.get_dummies(data, columns= ['DRIVER VISION', 'DRIVER ACTION','PHYSICAL CONDITION',
+        #        'ROADWAY CONDITION','DEVICE CONDITION', 'FIRST CRASH TYPE'], drop_first=True)
 
- 
+        
+        # input = pd.get_dummies(x, columns = CT, drop_first = True)
+        # #preds = pd.get_dummies(data, columns = CT, drop_first = True)
+        
+        # fname = 'scaler.sav'
+        # scaler = pickle.load(open(fname, 'rb'))
+        
 
+        # preds = scaler.fit_transform(data)
 
-        #preds =str(preds.idxmax(axis=1)[0])
-#########################workimg
+        # preds = np.array(preds)
+        # preds = preds.reshape(1,-1)
+   
+        # preds = model.predict(preds)
+
+        # preds = model_predict(input, model)
+        # preds = str(round(preds[0],2))
+
+       
+#########################workimg but ignore 
         # label=['FOLLOWING TOO CLOSELY','FAILING TO YIELD RIGHTOFWAY',
         #        'IMPROPER BACKING', 'SPEED RELATED', 'WEATHER',
         #        'DISREGARDING TRAFFIC SIGNALS', 'DISTRACTION  FROM INSIDE VEHICLE',
@@ -156,66 +298,8 @@ def upload():
 ##################################
     
 
-        return render_template('results.html', predictions=preds)
+        return render_template('results.html', predictions=result)
     return None
-
-
-# @app.route('/upload',methods=['GET'])
-# @cross_origin()
-# def predict():
-#     if request.method == 'GET':
-        
-#         #  reading the inputs given by the user
-#         speed = int(request.args['SPEED LIMIT'])
-#         age = int(request.args['AGE'])
-#         vision = request.args['DRIVER VISION']
-#         action = request.args['DRIVER ACTION']
-#         physical = request.args['PHYSICAL CONDITION']
-#         roadway = request.args['ROADWAY CONDITION']
-#         device = request.args['DEVICE CONDITION']
-#         fcrash = request.args['FIRST CRASH TYPE']
-        
-#         userInput = [[speed,age,vision,action,physical,roadway,device,fcrash]]
-        
-        
-#         model =  pickle.load(open('xgb1.pickle', 'rb')) # need to use as training model is onehot encoded
-        
-#         feature_selection=PCA()
-#         model = make_pipeline_imb
-# #  make a database
-# # asign a variable for cat
-# #  get onehotencoding
-# # 
-#         input = pd.DataFrame(userInput)
-#         input.columns = ['SPEED LIMIT', 'AGE','DRIVER VISION', 'DRIVER ACTION','PHYSICAL CONDITION',
-#                                 'ROADWAY CONDITION','DEVICE CONDITION','FIRST CRASH TYPE']
-#         CT = ['DRIVER VISION', 'DRIVER ACTION','PHYSICAL CONDITION',
-#                                 'ROADWAY CONDITION','DEVICE CONDITION','FIRST CRASH TYPE']
-
-#         input = pd.get_dummies(input, columns = CT, drop_first = True)
-       
-#         scaler.fit(input)
-#         input = scaler.transform(input)
-
-#         input = np.ravel(input)
-        
-#         input = np.array(input).reshape(1,-1)
-
-#         preds = model.predict(input.T)
-#         label=['FOLLOWING TOO CLOSELY','FAILING TO YIELD RIGHTOFWAY',
-#        'IMPROPER BACKING', 'SPEED RELATED', 'WEATHER',
-#        'DISREGARDING TRAFFIC SIGNALS', 'DISTRACTION  FROM INSIDE VEHICLE',
-#        'INTOXICATED/PHYSICAL CONDITION', 'IMPROPER LANE USAGE',
-#        'DISTRACTION  FROM OUTSIDE VEHICLE', 'IMPROPER TURNINGNO SIGNAL',
-#        'EQUIPMENT  VEHICLE CONDITION',
-#        'DRIVING SKILLSKNOWLEDGEEXPERIENCE', 'IMPROPER OVERTAKINGPASSING',
-#        'ROAD CONDITIONS']
-    
-#         pred_name = label[np.argmax(preds)]
-#         probability = preds[np.argmax(preds)]
-
-#         return render_template('results.html', prediction= probability)
-
 
 
 
